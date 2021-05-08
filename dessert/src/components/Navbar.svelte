6506b6de378c4fc8b161761d2ef8481fe0bb53a9
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-08 21:52:36 +0700

    remove unused ✏️

--- dessert/src/components/Navbar.svelte
+++ dessert/src/components/Navbar.svelte
@@ -31,16 +31,9 @@
         </svg>
       {/if}
     </button>
-    
-    <!-- <button class="bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-      Button
-    </button> -->
   </div>
 </header>
 
 <style>
-  input:checked ~ .dot {
-    transform: translateX(100%);
-    background-color: #fff;
-  }
+
 </style>
