e330d6c5f39bff43294fb28280df6905447db823
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-10 20:57:49 +0700

    add icon for navbar 🎀

--- dessert/src/icons/Homeicon.svelte
+++ dessert/src/icons/Homeicon.svelte
@@ -2,7 +2,7 @@
   export let className
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" class={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<svg class={className} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path
     stroke-linecap="round"
     stroke-linejoin="round"
