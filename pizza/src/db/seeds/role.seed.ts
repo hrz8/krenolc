2d14c7688a569b08596678196df0663c7343be09
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-15 21:12:59 +0700

    modify seed data role super admin 👨‍💼

--- pizza/src/db/seeds/role.seed.ts
+++ pizza/src/db/seeds/role.seed.ts
@@ -3,8 +3,8 @@ import { RoleInserPayload } from '../entities/role.entity'
 
 const data: RoleInserPayload[] = [
     {
-        name       : 'root',
-        description: 'super user access',
+        name       : 'superadmin',
+        description: 'super admin with root access as super user',
         permissions: [
             {
                 id: '2c4c6b4b-8b79-45f0-b975-972c89b5869b'
