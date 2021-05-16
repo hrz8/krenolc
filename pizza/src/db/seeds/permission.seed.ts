6c14f85ee0f14f009fc011ba8c48d3b04b760cfc
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-16 12:51:03 +0700

    add seed data for role 🌱

--- pizza/src/db/seeds/permission.seed.ts
+++ pizza/src/db/seeds/permission.seed.ts
@@ -5,6 +5,16 @@ const data: PermissionInserPayload[] = [
         id         : '2c4c6b4b-8b79-45f0-b975-972c89b5869b',
         name       : 'root',
         description: 'super user access'
+    },
+    {
+        id         : '932ca262-198c-4991-bca5-974d3b93c4d2',
+        name       : 'getAll@faq',
+        description: 'get all faq data'
+    },
+    {
+        id         : 'b5ba6d92-8fac-4842-9d60-f03c43f3dc65',
+        name       : 'getDefault@faq',
+        description: 'get default faq data'
     }
 ]
 
--- pizza/src/db/seeds/role.seed.ts
+++ pizza/src/db/seeds/role.seed.ts
@@ -10,6 +10,18 @@ const data: RoleInserPayload[] = [
                 id: '2c4c6b4b-8b79-45f0-b975-972c89b5869b'
             } as Permission
         ]
+    },
+    {
+        name       : 'user',
+        description: 'just a user ☹️',
+        permissions: [
+            {
+                id: '932ca262-198c-4991-bca5-974d3b93c4d2'
+            } as Permission,
+            {
+                id: 'b5ba6d92-8fac-4842-9d60-f03c43f3dc65'
+            } as Permission
+        ]
     }
 ]
 
