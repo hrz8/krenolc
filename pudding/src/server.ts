792c3a5d8c2b6e4dea59122d57d98959cef5ec03
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-22 21:48:14 +0700

    add nats factory 🪓

--- pudding/src/server.ts
+++ pudding/src/server.ts
@@ -1,22 +1,19 @@
-import {
-    Codec, connect as natsConnect, JSONCodec as natsJSONCodec, Subscription
-} from 'nats'
+import NatsFactory from './utils/nats'
 
-const listen = async (subscribtion: Subscription, codec: Codec<any>) => {
-    for await (const m of subscribtion) {
-        console.log(`subject: ${m.subject}`)
-        console.log(`data: ${JSON.stringify(codec.decode(m.data))}`)
+const natsWrapper = async () => {
+
+}
+
+const main = async () => {
+    await NatsFactory.init()
+    const natsConn = NatsFactory.getConnection()
+    const auditSubscription = natsConn.subscribe('audit')
+    const subscriptions = [auditSubscription]
+    for await (const sub of subscriptions) {
+        await NatsFactory.listen(sub)
     }
 }
 
 (async function () {
-    const nc = await natsConnect({
-        servers: '127.0.0.1:4222'
-    })
-    console.log(`connected to ${nc.getServer()}`)
-
-    const codec = natsJSONCodec()
-    const subscribtion = nc.subscribe('audit')
-
-    await listen(subscribtion, codec)
+    await main()
 }())
