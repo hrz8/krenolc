cb9fb241c7025ae9f256a9b70f757aaa1fc70c3a
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-22 20:30:55 +0700

    add minimum nats connection 🔌

--- pudding/src/server.ts
+++ pudding/src/server.ts
@@ -1,1 +1,19 @@
-console.log('OI')
+import { connect as natsConnect } from 'nats'
+
+const main = async () => {
+    const nc = await natsConnect({
+        servers: '127.0.0.1:4222'
+    })
+    console.log(`connected to ${nc.getServer()}`)
+    const done = nc.closed()
+
+    await nc.close()
+    const err = await done
+    if (err) {
+        console.log('error closing:', err)
+    }
+}
+
+(async function () {
+    await main()
+}())
