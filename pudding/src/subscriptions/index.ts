629a763a2857bc3c727ec63b4baddbc5bf3543a3
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-26 15:00:10 +0700

    refactor factory 🏭

--- pudding/src/subscriptions/index.ts
+++ pudding/src/subscriptions/index.ts
@@ -1,11 +1,16 @@
-import { NatsConnection } from 'nats'
-import { SubscriptionObject } from '../types/subscription'
+import { SubscriptionHandler } from '../types/subscription'
 import auditSub from './audit'
 
-const subscriptions = [auditSub]
-
-export default (conn: NatsConnection): SubscriptionObject[] => subscriptions
-    .map((obj) => ({
-        sub    : conn.subscribe(obj.sub),
-        handler: obj.handler
+const subs = {
+    ...auditSub
+}
+const subscriptions: {
+    subject: string;
+    handler: SubscriptionHandler
+}[] = Object.keys(subs)
+    .map((sub) => ({
+        subject: sub,
+        handler: subs[sub].handler
     }))
+
+export default subscriptions
--- pudding/src/utils/nats.ts
+++ pudding/src/utils/nats.ts
@@ -1,63 +1,35 @@
 import {
-    Codec, connect as natsConnect, JSONCodec as natsJSONCodec, Subscription, NatsConnection
+    NatsConnection, Codec, connect as natsConnect, JSONCodec as natsJSONCodec
 } from 'nats'
-import getNatsSubscriptions from '../subscriptions'
-import log from './logger'
-import { SubscriptionHandler, SubscriptionObject } from '../types/subscription'
+import { Subscription } from '../types/subscription'
+import subscriptions from '../subscriptions'
 
-interface SubsObj { subs: Subscription[], handlers: SubscriptionHandler[] }
 export default class NatsFactory {
     private static connection: NatsConnection
 
     private static codec: Codec<any>
 
     public static async init(): Promise<void> {
+        // core props
         this.connection = await natsConnect({
-            servers: process.env.NATS_SERVER
+            servers: 'localhost:4222'
         })
         this.codec = natsJSONCodec()
-        const subObjArr = getNatsSubscriptions(this.connection)
-        const subObj = subObjArr.reduce((
-            acc: SubsObj,
-            curr: SubscriptionObject
-        ): SubsObj => {
-            const { sub, handler } = curr
-            return {
-                subs    : [...acc.subs, sub],
-                handlers: [...acc.handlers, handler]
-            }
-        }, {
-            subs    : [],
-            handlers: []
-        } as SubsObj)
 
-        // listen wrapper
-        this.listener(subObj.subs, subObj.handlers)
+        // register all subscriptions
+        this.register(subscriptions)
     }
 
-    private static async listener(
-        subs: Subscription[],
-        handlers: SubscriptionHandler[]
+    private static async register(
+        subs: Subscription[]
     ): Promise<void> {
-        return new Promise((res, rej) => {
-            try {
-                for (const [i, sub] of subs.entries()) {
-                    this.listen(sub, handlers[i])
-                }
-            } catch (err) {
-                log.error('error NatsFactory.listen')
-                log.error(err)
-                rej(err)
-            }
-        })
-    }
-
-    private static async listen(
-        sub: Subscription,
-        handler: SubscriptionHandler
-    ): Promise<void> {
-        for await (const msg of sub) {
-            handler(this.codec, msg)
+        for await (const subcription of subs) {
+            await this.connection.subscribe(subcription.subject, {
+                callback: (err, msg) => subcription.handler(err, {
+                    sub : msg.subject,
+                    data: this.codec.decode(msg.data) as Record<string, any>
+                })
+            })
         }
     }
 
