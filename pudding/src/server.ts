bf8b4e353dc10b531cfb368564fa1ac1ee5b6f6f
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-26 15:37:31 +0700

    add error log ❗️

--- pudding/src/server.ts
+++ pudding/src/server.ts
@@ -1,11 +1,16 @@
+import log from './utils/logger'
 import Nats from './utils/nats'
 
 const main = async () => {
     const natsServer = 'localhost:4222'
     const nats = Nats(natsServer)
 
-    nats.listen(() => {
-        console.log('listening to: ', natsServer)
+    nats.listen((err) => {
+        if (!err) {
+            console.log('listening to: ', natsServer)
+            return
+        }
+        log.error(`encountered nats.listen error: ${err.message}`)
     })
 }
 
