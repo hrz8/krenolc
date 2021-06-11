c59471d483e03d6486968975214a452ecb8488fd
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-06-11 22:37:05 +0700

    add material ui 💄💋

--- dessert/src/components/Home/LogoutButton.svelte
+++ dessert/src/components/Home/LogoutButton.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Button from '@smui/button'
+  import Label from '@smui/common'
   import AuthFactory from '@services/auth'
 
   function logout() {
@@ -6,6 +8,13 @@
   }
 </script>
 
-<button class="px-4 py-1 text-white bg-yellow-500 rounded-full font-semibold hover:bg-yellow-700 focus:outline-none" on:click="{logout}">
+<!-- <button class="px-4 py-1 text-white bg-yellow-500 rounded-full font-semibold hover:bg-yellow-700 focus:outline-none" on:click="{logout}">
   logout Button
-</button>
+</button> -->
+
+<Button
+  variant="outlined"
+  class="button-shaped-round"
+>
+  <Label>Outlined</Label>
+</Button>
