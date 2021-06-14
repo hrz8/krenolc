4a3e0c7879c5815e7f657c4673a4cfb6be6d6cb6
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-06-14 19:25:13 +0700

    change indentation to spaces 🐓

--- doughnut/src/components/sidebar.svelte
+++ doughnut/src/components/sidebar.svelte
@@ -1,75 +1,75 @@
 
 <script lang="ts">
-    import { Icon, Tooltip, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'sveltestrap/src';
+  import { Icon, Tooltip, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'sveltestrap/src';
 </script>
 
-<div class="d-flex flex-column flex-shrink-0" style="width: 4.5em; height: 100vh; overflow-y: auto; box-shadow: 0 0.125em 0.5em rgb(0 0 0 / 15%);">
-    <a href="/#" id="sidebar-app" class="text-center p-3 link-dark text-decoration-none">
-        <Icon class="h3" name="controller" />
-    </a>
-    <Tooltip target="sidebar-app" placement="right">Krenolc Dashboard</Tooltip>
-    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-        <li>
-            <a href="/#" id="sidebar-dashboard" class="nav-link active py-3 border-bottom">
-                <Icon class="h4" name="speedometer2" />
-            </a>
-            <Tooltip target="sidebar-dashboard" placement="right">Dashboard</Tooltip>
-        </li>
-        <li>
-            <a href="/#" id="sidebar-editor" class="nav-link py-3 border-bottom">
-                <Icon class="h4" name="pencil-square" />
-            </a>
-            <Tooltip target="sidebar-editor" placement="right">Editor</Tooltip>
-        </li>
-        <li>
-            <a href="/#" id="sidebar-faq" class="nav-link py-3 border-bottom">
-                <Icon class="h4" name="question-circle" />
-            </a>
-            <Tooltip target="sidebar-faq" placement="right">FAQ</Tooltip>
-        </li>
-        <li>
-            <a href="/#" id="sidebar-livechat" class="nav-link py-3 border-bottom">
-                <Icon class="h4" name="chat-square-dots" />
-            </a>
-            <Tooltip target="sidebar-livechat" placement="right">Live Chat</Tooltip>
-        </li>
-        <li>
-            <a href="/#" id="sidebar-widget" class="nav-link py-3 border-bottom">
-                <Icon class="h4" name="tv" />
-            </a>
-            <Tooltip target="sidebar-widget" placement="right">Widget</Tooltip>
-        </li>
-        <li>
-            <a href="/#" id="sidebar-settings" class="nav-link py-3 border-bottom">
-                <Icon class="h4" name="sliders" />
-            </a>
-            <Tooltip target="sidebar-settings" placement="right">Settings</Tooltip>
-        </li>
-    </ul>
-    <Dropdown class="border-top" direction="right" style="position: unset;">
-        <DropdownToggle tag="div">
-            <a href="/#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none">
-                <Icon class="h4" name="person-circle" />
-            </a>
-        </DropdownToggle>
-        <DropdownMenu class="text-small shadow">
-            <DropdownItem header>Account</DropdownItem>
-            <DropdownItem>
-                <Icon class="h4" name="person-badge" /> Profile
-            </DropdownItem>
-            <DropdownItem>
-                <Icon class="h4" name="gear" /> Settings
-            </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>
-                <Icon class="h4" name="box-arrow-right" /> Sign Out
-            </DropdownItem>
-        </DropdownMenu>
-    </Dropdown>
+<div class="d-flex flex-column flex-shrink-0" style="width: 3.5em; height: 100vh; overflow-y: auto; box-shadow: 0 0.125em 0.5em rgb(0 0 0 / 15%);">
+  <a href="/#" id="sidebar-app" class="text-center p-3 link-dark text-decoration-none">
+    <Icon class="h3" name="controller" />
+  </a>
+  <Tooltip target="sidebar-app" placement="right">Krenolc Dashboard</Tooltip>
+  <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+    <li>
+      <a href="/#" id="sidebar-dashboard" class="nav-link active py-3 border-bottom">
+        <Icon class="h4" name="speedometer2" />
+      </a>
+      <Tooltip target="sidebar-dashboard" placement="right">Dashboard</Tooltip>
+    </li>
+    <li>
+      <a href="/#" id="sidebar-editor" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="pencil-square" />
+      </a>
+      <Tooltip target="sidebar-editor" placement="right">Editor</Tooltip>
+    </li>
+    <li>
+      <a href="/#" id="sidebar-faq" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="question-circle" />
+      </a>
+      <Tooltip target="sidebar-faq" placement="right">FAQ</Tooltip>
+    </li>
+    <li>
+      <a href="/#" id="sidebar-livechat" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="chat-square-dots" />
+      </a>
+      <Tooltip target="sidebar-livechat" placement="right">Live Chat</Tooltip>
+    </li>
+    <li>
+      <a href="/#" id="sidebar-widget" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="tv" />
+      </a>
+      <Tooltip target="sidebar-widget" placement="right">Widget</Tooltip>
+    </li>
+    <li>
+      <a href="/#" id="sidebar-settings" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="sliders" />
+      </a>
+      <Tooltip target="sidebar-settings" placement="right">Settings</Tooltip>
+    </li>
+  </ul>
+  <Dropdown class="border-top" direction="right" style="position: unset;">
+    <DropdownToggle tag="div">
+      <a href="/#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none">
+        <Icon class="h4" name="person-circle" />
+      </a>
+    </DropdownToggle>
+    <DropdownMenu class="text-small shadow">
+      <DropdownItem header>Account</DropdownItem>
+      <DropdownItem>
+        <Icon class="h4" name="person-badge" /> Profile
+      </DropdownItem>
+      <DropdownItem>
+        <Icon class="h4" name="gear" /> Settings
+      </DropdownItem>
+      <DropdownItem divider />
+      <DropdownItem>
+        <Icon class="h4" name="box-arrow-right" /> Sign Out
+      </DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
 </div>
 
 <style>
-    .nav-flush .nav-link {
-        border-radius: 0;
-    }
+  .nav-flush .nav-link {
+    border-radius: 0;
+  }
 </style>
--- doughnut/src/routes/__layout.svelte
+++ doughnut/src/routes/__layout.svelte
@@ -1,42 +1,41 @@
 <script lang="ts">
-    import { Styles } from 'sveltestrap';
-    import { Button } from 'sveltestrap/src'
-    import type { ButtonColor } from 'sveltestrap/src/Button'
+  import { Styles } from 'sveltestrap';
+  import { Button } from 'sveltestrap/src'
+  import type { ButtonColor } from 'sveltestrap/src/Button'
 
-    import Sidebar from '../components/sidebar.svelte'
+  import Sidebar from '../components/sidebar.svelte'
 
-    const colors: ButtonColor[] = [
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark'
-    ]
+  const colors: ButtonColor[] = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark'
+  ]
 </script>
 
 <Styles />
 
-<main style="display: flex;">
-    <Sidebar />
-    <div class="container-fluid">
-        <div class="row">
-        <!-- <div class="col-sm p-3 min-vh-100"></div> -->
-            <slot></slot>
-        </div>
+<main>
+  <Sidebar />
+  <div class="container-fluid">
+    <div class="row">
+      <slot></slot>
     </div>
+  </div>
 </main>
 
 <style>
-    main {
-        display: flex;
-        flex-wrap: nowrap;
-        height: 100vh;
-        height: -webkit-fill-available;
-        max-height: 100vh;
-        overflow-x: auto;
-        overflow-y: hidden;
-    }
+  main {
+    display: flex;
+    flex-wrap: nowrap;
+    height: 100vh;
+    height: -webkit-fill-available;
+    max-height: 100vh;
+    overflow-x: auto;
+    overflow-y: hidden;
+  }
 </style>
