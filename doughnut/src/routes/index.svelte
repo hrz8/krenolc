77f24acda07fef6ad97be2735787a7c98d22fd8f
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-07-11 12:49:49 +0700

    add button for debug 🕵️‍♂️

--- doughnut/src/routes/index.svelte
+++ doughnut/src/routes/index.svelte
@@ -1,2 +1,28 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  import { Button } from 'sveltestrap'
+
+  import { accessToken as accessTokenStore } from '../stores/auth'
+
+
+  let token = ''
+
+  onMount(async() => {
+    accessTokenStore.subscribe((data) => {
+      token = data
+    })
+  })
+
+  function showCredential() {
+    console.debug({
+      token
+    })
+  }
+</script>
+
 <h1>Welcome to Krenolc</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>This should be dashboard but you know....</p>
+<div>
+  <Button color="primary" on:click={showCredential}>Credentials</Button>
+</div>
