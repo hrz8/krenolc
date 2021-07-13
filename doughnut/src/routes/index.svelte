c431ef1a618148beb26ecd548b97ad77c4d615d5
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-07-13 21:04:11 +0700

    remove creds from index 👙

--- doughnut/src/routes/index.svelte
+++ doughnut/src/routes/index.svelte
@@ -1,28 +1,16 @@
 <script lang="ts">
   import { onMount } from 'svelte'
 
-  import { Button } from 'sveltestrap'
-
   import { accessToken as accessTokenStore } from '../stores/auth'
-
-
-  let token = ''
 
   onMount(async() => {
     accessTokenStore.subscribe((data) => {
-      token = data
+      console.debug({
+        token: data
+      })
     })
   })
-
-  function showCredential() {
-    console.debug({
-      token
-    })
-  }
 </script>
 
 <h1>Welcome to Krenolc</h1>
 <p>This should be dashboard but you know....</p>
-<div>
-  <Button color="primary" on:click={showCredential}>Credentials</Button>
-</div>
