9ada2707035177d49189e8e119748fe92fe83285
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-07-18 13:40:32 +0700

    upgrade sveltestrap 👟

--- doughnut/src/components/navbar.svelte
+++ doughnut/src/components/navbar.svelte
@@ -2,22 +2,21 @@
   import {
     Navbar,
     NavbarBrand,
-    ButtonDropdown,
-    DropdownToggle,
-    DropdownMenu,
     DropdownItem
-  } from 'sveltestrap'
+  } from 'sveltestrap/src'
 </script>
 
 <div>
   <Navbar color="light" light expand="md">
     <NavbarBrand href="/">👑 krenolc</NavbarBrand>
-    <ButtonDropdown>
-      <DropdownToggle color="dark" caret>(hirzi-bot)</DropdownToggle>
-      <DropdownMenu>
+    <div class="dropdown">
+      <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuBot" data-bs-toggle="dropdown" aria-expanded="false">
+        (hirzi-bot)
+      </button>
+      <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenuBot">
         <DropdownItem header>Available Bots</DropdownItem>
         <DropdownItem>hirzi-bot</DropdownItem>
-      </DropdownMenu>
-    </ButtonDropdown>
+      </ul>
+    </div>
   </Navbar>
 </div>
