44bc265677c7bddd0fedb41fe7f60f09c794646d
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-08-04 20:44:51 +0700

    add card  🃏

--- doughnut/src/routes/editor/index.svelte
+++ doughnut/src/routes/editor/index.svelte
@@ -1,4 +1,7 @@
-<script>
+<script lang="ts">
+  import {
+    TabContent, TabPane, Col, Container, Row, Card, CardBody
+  } from 'sveltestrap/src'
   import Tree from '../../components/editor/tree.svelte'
 
   const trees = [
@@ -35,6 +38,29 @@
   <title>Editor</title>
 </svelte:head>
 
-{#each trees as tree}
-  <Tree {tree} />
-{/each}
+<Container class="mt-4">
+  <Row>
+    <Col lg="4">
+      <Card>
+        <CardBody>
+          <TabContent>
+            <TabPane tabId="node" tab="Node" style="height: 75vh; overflow-y: auto;" active>
+              <div class="mt-3">
+                {#each trees as tree}
+                  <Tree {tree} />
+                {/each}
+              </div>
+            </TabPane>
+            <TabPane tabId="trigger" tab="Trigger" style="height: 75vh; overflow-y: auto;" >
+              <div class="mt-3">
+                {#each trees as tree}
+                  <Tree {tree} />
+                {/each}
+              </div>
+            </TabPane>
+          </TabContent>
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
+</Container>
