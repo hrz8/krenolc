05e2532be7aa9eba11f06f2a8b3eed5abbb61dea
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-08-10 21:00:27 +0700

    small modif init method 🐠

--- dashboard-ui/store/index.ts
+++ dashboard-ui/store/index.ts
@@ -1,8 +1,9 @@
 import { getModule } from 'nuxt-property-decorator'
 import { Store } from 'vuex'
-import User from '~/store/users'
+import Users from '~/store/users'
 
-const initializer = (store: Store<any>) => ({
-  user: getModule(User, store)
-})
-export const plugins = [initializer]
+export const plugins = [
+  (store: Store<any>) => ({
+    users: getModule(Users, store)
+  })
+]
